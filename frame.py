11c18e30be85e17e5b24bec6edda12b971918950
Chris Lapa <chris@lapa.com.au>
2016-12-16 11:18:01 +0000

    frame: adds common frame class.
    Frame class is transferred over the wire between core and webserver.
    
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

---
+++ frame.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+class Frame(object):
+    def __init__(self, frame, timestamp, fps):
+        self.frame = frame
+        self.timestamp = timestamp
+        self.fps = fps
+
