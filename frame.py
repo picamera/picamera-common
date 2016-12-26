511135481d081d6bb66bf0bcdc1903e11fbdec21
Chris Lapa <chris@lapa.com.au>
2016-12-26 11:09:42 +0000

    frame: adds addition fields to Frame for usage in video saving.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- frame.py
+++ frame.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 class Frame(object):
-    def __init__(self, frame, timestamp, fps):
+    def __init__(self, frame, timestamp, ideal_fps, real_fps, xres, yres):
         self.frame = frame
         self.timestamp = timestamp
-        self.fps = fps
+        self.ideal_fps = ideal_fps
+        self.real_fps = real_fps
+        self.xres = xres
+        self.yres = yres
 
