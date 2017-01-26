dd28925a4a3fa66ec27c5188b03fc30c2713454d
Chris Lapa <chris@lapa.com.au>
2017-01-26 11:31:06 +0000

    frame: adds support for format argument required for new grabber types.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- frame.py
+++ frame.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 class Frame(object):
-    def __init__(self, frame, timestamp, ideal_fps, real_fps, xres, yres):
+    def __init__(self, frame, format, timestamp, ideal_fps, real_fps, xres, yres):
         self.frame = frame
+        self.format = format
         self.timestamp = timestamp
         self.ideal_fps = ideal_fps
         self.real_fps = real_fps
         self.xres = xres
         self.yres = yres
-
